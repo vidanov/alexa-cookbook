6368bd8308ddae1b54d6fa5c278ab2133be4e27f
Alexey Vidanov <vidanov@gmail.com>
2017-10-17 20:42:55 +0200

    Update index.js

--- labs/Day-1/0-hello-world/src/index.js
+++ labs/Day-1/0-hello-world/src/index.js
@@ -21,7 +21,7 @@ var handlers = {
   },
 
   'HelloWorldIntent': function () {
-      this.response.speak('Die Verwendung des Textes „Hello World!“, der auch durch einen beliebigen Text ersetzt werden kann, aber dennoch gerne unverändert benutzt wird, ist eine Tradition und geht auf ein internes Programmierhandbuch der Bell Laboratories über die Programmiersprache C zurück, das Brian Kernighan 1974 verfasste, nachdem er dort schon ein Jahr zuvor die Wörter „hello“ und „world“ in einer Einführung in die Programmiersprache B verwendet hatte.')
+      this.response.speak('Ein Hallo-Welt-Programm ist ein kleines Computerprogramm, das auf möglichst einfache Weise zeigen soll, welche Anweisungen oder Bestandteile für ein vollständiges Programm in einer Programmiersprache benötigt werden, und somit einen ersten Einblick in die Syntax gibt. Aufgabe des Programms ist, den Text Hallo Welt! oder auf Englisch Hello World! auszugeben. Wegen der einfachen Aufgabenstellung eignen sich solche Programme insbesondere für didaktische Zwecke. Deshalb wird es in vielen Programmier-Lehrbüchern als Einsteigerprogramm verwendet. Die Verwendung des Textes „Hello World!“, der auch durch einen beliebigen Text ersetzt werden kann, aber dennoch gerne unverändert benutzt wird, ist eine Tradition und geht auf ein internes Programmierhandbuch der Bell Laboratories über die Programmiersprache C zurück, das Brian Kernighan 1974 verfasste, nachdem er dort schon ein Jahr zuvor die Wörter „hello“ und „world“ in einer Einführung in die Programmiersprache B verwendet hatte.')
       // aus https://de.wikipedia.org/wiki/Hallo-Welt-Programm
       this.emit(':responseReady');
   }
